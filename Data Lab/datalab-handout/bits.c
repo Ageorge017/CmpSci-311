28510aee64a798677a6455dce8bc88f94f8b5067
Albert George <37078002+Ageorge017@users.noreply.github.com>
2018-09-21 23:01:45 -0400

    Update bits.c

--- Data Lab/datalab-handout/bits.c
+++ Data Lab/datalab-handout/bits.c
@@ -1,7 +1,6 @@
 /* 
  * CS:APP Data Lab 
  * 
- * ALBERT GEORGE atg5215
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
